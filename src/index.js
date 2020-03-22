32ae09849522fc082d2d948f16c8c26855874094
Ergashev Ulug`bek <57306599+ErgashevUlugbek@users.noreply.github.com>
2020-03-22 19:01:50 +0500

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const MORSE_TABLE = {
+const TABLE = {
     '.-':     'a',
     '-...':   'b',
     '-.-.':   'c',
@@ -39,6 +39,26 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let words = expr.split('**********');
+    let letters = [];
+    let regexZeros = /00/g;
+    let regexLetters = /.{1,10}/g;
+    let result =[];
+    for(let i = 0; i< words.length; i++){
+        letters.push(words[i].match(regexLetters));
+        for(let l =0; l<letters.length; l++){
+          letters[l] = letters[l].toString().replace(regexZeros, '');
+          letters[l] = letters[l].toString().replace(/10/g, '.');
+          letters[l] = letters[l].toString().replace(/11/g, '-');
+          letters[l] = letters[l].split(',').map(element => element = element.replace(element,TABLE[element])).join('');
+          
+        }
+        result[i] = letters.join('');
+        
+        letters = [];
+    }
+    
+    return result.join(' ');
 }
 
 module.exports = {
